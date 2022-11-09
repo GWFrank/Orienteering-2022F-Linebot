f390f56724f870c2ca96ffb1990b42fcd1254220
Frank Kuo <frankkuo@protonmail.com>
2022-11-09 18:43:27 +0800

    update bot

--- bot/bot.py
+++ bot/bot.py
@@ -37,7 +37,7 @@ class Station:
         return (sid == self._sid and flag == self._flag)
 
     def get_hints(self) -> tuple[str, str]:
-        return self.hints
+        return self._hints
     
     def get_points(self) -> tuple[bool, int]:
         if self._captured:
@@ -46,31 +46,28 @@ class Station:
             self._captured = True
             return (True, round(1.5*self._points))
 
-def dict2station(objs):
-    sts = list()
-    for obj in objs:
-        sts.append(Station(
-            obj["sid"],
-            obj["name"],
-            obj["hints"],
-            obj["points"],
-            obj["flag"]
-        ))
-    return sts
+def dict2station(obj):
+    return Station(
+        obj["_sid"],
+        obj["_name"],
+        obj["_hints"],
+        obj["_points"],
+        obj["_flag"]
+    )
 
 class Team:
-    def __init__(self, user_id: str, name: str):
+    def __init__(self, user_id: str, name: str, score=0, answered_stations=list(), captured_stations=list()):
         self._user_id = user_id
         self._name = name
-        self.score = 0
-        self._answered_stations = list()
-        self._captured_stations = list()
+        self.score = score
+        self._answered_stations = answered_stations
+        self._captured_stations = captured_stations
     
     def change_name(self, new_name: str) -> None:
         self._name = new_name
     
     def get_info(self) -> str:
-        return f"{self._name} | {self.score}"
+        return f"\n{self._name} | {self.score}"
 
     def check_answered(self, sid: int) -> bool:
         return sid in self._answered_stations
@@ -84,10 +81,25 @@ class Team:
     def captured(self, sid: int) -> None:
         self._captured_stations.append(sid)
 
-
 TeamList = dict[str, Team]
 StationList = list[Station]
-Teams = TeamList()
+
+def dict2team(obj):
+    return Team(
+        obj["_user_id"],
+        obj["_name"],
+        obj["score"],
+        obj["_answered_stations"],
+        obj["_captured_stations"]
+    )
+
+try:
+    with open("team.json", "r") as f:
+        Teams = json.load(f)
+        for k, v in Teams.items():
+            Teams[k] = dict2team(v)
+except FileNotFoundError:
+    Teams = TeamList()
 # Stations = StationList()
 with open("stations.json", "r") as f:
     Stations = json.load(f, object_hook=dict2station)
@@ -126,10 +138,9 @@ def getCommand(event: MessageEvent):
         if len(args) != 2:
             msgs.append(TextSendMessage(
                 text = """格式錯誤。
-                指令格式： register <team name>
-                範例： register 第一組
-                備註： 隊伍名稱不能含有空白、換行
-                """
+指令格式： register <team name>
+範例： register 第一組
+備註： 隊伍名稱不能含有空白、換行"""
             ))
         else:
             uid = event.source.user_id
@@ -141,15 +152,15 @@ def getCommand(event: MessageEvent):
                 text = f"已成功註冊為「{args[1]}」"
             ))
             
-            tmp_team = list()
-            for t in Teams:
-                tmp_team.append(vars(Teams[t]))
+            tmp_team = dict()
+            for k, v in Teams.items():
+                tmp_team[k] = vars(v)
             with open("team.json", "w", newline='', encoding="utf-8") as json_file:
                 json.dump(tmp_team, json_file, ensure_ascii=False)
 
     elif cmd == "leaderboard":
         # Show leaderboard & status of each question
-        leaderboard_msg = "隊伍名稱 | 分數\n"
+        leaderboard_msg = "隊伍名稱 | 分數"
         for t in sorted(Teams.values(), key=lambda x:x.score, reverse=True):
             leaderboard_msg += t.get_info()
         msgs.append(TextSendMessage(text=leaderboard_msg))
@@ -158,10 +169,9 @@ def getCommand(event: MessageEvent):
         if len(args) != 3 or not args[1].isdigit():
             msgs.append(TextSendMessage(
                 text = """格式錯誤。
-                指令格式： answer <station_id> <your_answer>
-                範例： answer 1 定向越野好讚
-                備註： <station_id> 須為數字，答案不能含有空白、換行
-                """
+指令格式： answer <station_id> <your_answer>
+範例： answer 1 定向越野好讚
+備註： <station_id> 須為數字，答案不能含有空白、換行"""
             ))
         else:
             uid = event.source.user_id
@@ -174,12 +184,17 @@ def getCommand(event: MessageEvent):
                 for t in Stations:
                     if t.check_answer(sid, name):
                         hint = t.get_hints()
-                        msgs.append(
+                        msgs += [
                             TextSendMessage(text="恭喜你答對了，以下是你的站內地圖以及要找的地方。"),
                             ImageSendMessage(original_content_url=hint[0]),
                             ImageSendMessage(original_content_url=hint[1])
-                        )
+                        ]
                         Teams[uid].answered(sid)
+                        tmp_team = dict()
+                        for k, v in Teams.items():
+                            tmp_team[k] = vars(v)
+                        with open("team.json", "w", newline='', encoding="utf-8") as json_file:
+                            json.dump(tmp_team, json_file, ensure_ascii=False)
                 else: # When no station matched
                     msgs.append(TextSendMessage(
                         text = "也許你好像哪裡答錯了QQ"
@@ -190,15 +205,18 @@ def getCommand(event: MessageEvent):
         if len(args) != 3 or not args[1].isdigit():
             msgs.append(TextSendMessage(
                 text = """格式錯誤。
-                指令格式： capture <station_id> <your_flag>
-                範例： capture 1 flag\{定向越野好讚\}
-                備註： <station_id> 須為數字，<your_flag>為一個形式為flag\{<content>\}的字串
-                """
+指令格式： capture <station_id> <your_flag>
+範例： capture 1 flag\{定向越野好讚\}
+備註： <station_id> 須為數字，<your_flag>為一個形式為flag\{<content>\}的字串"""
             ))
         else:
             uid = event.source.user_id
             sid, flag = int(args[1]), args[2]
-            if Teams[uid].check_captured(sid):
+            if not Teams[uid].check_answered(sid):
+                msgs.append(TextSendMessage(
+                    text = "你好像還沒解出這是哪一站喔"
+                ))
+            elif Teams[uid].check_captured(sid):
                 msgs.append(TextSendMessage(
                     text = "再傳一次是不會獲得兩倍分數的"
                 ))
@@ -211,17 +229,20 @@ def getCommand(event: MessageEvent):
                         if first:
                             msgs.append(TextSendMessage(
                                 text = f"""恭喜你獲得本站的首殺
-                                獲得分數{pts}
-                                目前總得分{Teams[uid].score}
-                                """
+獲得分數{pts}
+目前總得分{Teams[uid].score}"""
                             ))
                         else:
                             msgs.append(TextSendMessage(
                                 text = f"""恭喜你獲得本站的flag
-                                獲得分數{pts}
-                                目前總得分{Teams[uid].score}
-                                """
+獲得分數{pts}
+目前總得分{Teams[uid].score}"""
                             ))
+                        tmp_team = dict()
+                        for k, v in Teams.items():
+                            tmp_team[k] = vars(v)
+                        with open("team.json", "w", newline='', encoding="utf-8") as json_file:
+                            json.dump(tmp_team, json_file, ensure_ascii=False)
                 else:
                     msgs.append(TextSendMessage(
                         text = "flag好像不是這個QQ，請檢查看看有沒有寫錯字"
